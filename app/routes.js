67b082c5ebcdb33a15a64d6cb99255ef09e38d2f
kim-flores <kimflores.k@gmail.com>
2020-09-25 23:24:55 -0400

    first commit

--- app/routes.js
+++ app/routes.js
@@ -1,44 +1,63 @@
+var ObjectID = require('mongodb').ObjectID
 module.exports = function(app, passport, db) {
 
   // normal routes ===============================================================
-  
+
       // show the home page (will also have our login links)
       app.get('/', function(req, res) {
           res.render('index.ejs');
       });
-  
+
       // PROFILE SECTION =========================
       app.get('/profile', isLoggedIn, function(req, res) {
-          db.collection('messages').find().toArray((err, result) => {
+          db.collection('data').find().toArray((err, result) => {
             if (err) return console.log(err)
             res.render('profile.ejs', {
               user : req.user,
-              messages: result
+              data: result
             })
           })
       });
-  
+      // casino staff login retireves total
+
+   // get casion id to use in the body
+
+
+      // app.get('/staff', function(req, res) {
+      // db.collection('data').find(total)(err, result) => {
+      //   console.log(result)
+      //   if (err) return console.log(err)
+      //   res.render('staff.ejs', {
+      //     messages: result
+      //   })
+      // })
+
       // LOGOUT ==============================
       app.get('/logout', function(req, res) {
           req.logout();
           res.redirect('/');
       });
-  
+
   // message board routes ===============================================================
-  
-      app.post('/messages', (req, res) => {
-        db.collection('messages').save({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
+
+      app.post('/game', (req, res) => {
+        db.collection('data').save({
+          casinoTotal: req.body.casinoTotal,
+          wins: req.body.wins,
+          losses: req.body.losses}
+           , (err, result) => {
           if (err) return console.log(err)
           console.log('saved to database')
-          res.redirect('/profile')
+          res.redirect('/')
         })
       })
-  
-      app.put('/thumbUp', (req, res) => {
-        db.collection('messages')
-        .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+      app.put('/game', (req, res) => {
+        db.collection('data')
+        .updateMany({wins: req.body.wins, losses: req.body.losses, casinoTotal: req.body.casinoTotal}, {
           $set: {
-            thumbUp:req.body.thumbUp + 1
+            wins: req.body.wins,
+            losses: req.body.losses,
+            casinoTotal: req.body.casinoTotal,
           }
         }, {
           sort: {_id: -1},
@@ -48,66 +67,81 @@ module.exports = function(app, passport, db) {
           res.send(result)
         })
       })
-      app.put('/thumbDown', (req, res) => {
-        db.collection('messages')
-        .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
-          $set: {
-            thumbUp:req.body.thumbUp - 1
-          }
-        }, {
-          sort: {_id: -1},
-          upsert: true
-        }, (err, result) => {
-          if (err) return res.send(err)
-          res.send(result)
-        })
-      })
-  
-      app.delete('/messages', (req, res) => {
-        db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
-          if (err) return res.send(500, err)
-          res.send('Message deleted!')
-        })
-      })
-  
+
+//       db.grades.findOneAndUpdate(
+//    { "name" : "R. Stiles" },
+//    { $inc: { "points" : 5 } }
+// )
+
+      // app.put('/thumbDown', (req, res) => {
+      //   db.collection('data')
+      //   .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+      //     $set: {
+      //       thumbUp:req.body.thumbUp - 1
+      //     }
+      //   }, {
+      //     sort: {_id: -1},
+      //     upsert: true
+      //   }, (err, result) => {
+      //     if (err) return res.send(err)
+      //     res.send(result)
+      //   })
+      // })
+      // app.put('/game', (req, res) => {
+      //   db.collection('data')
+      //   .findOneAndUpdate({_id: new ObjectId(req.body.id)}, {
+      //     $inc: {
+      //       wins: req.body.wins,
+      //       losses: req.body.losses,
+      //       money: req.body.money,
+      //     }
+      //   }, {
+      //     sort: {_id: -1},
+      //     upsert: true
+      //   }, (err, result) => {
+      //     if (err) return res.send(err)
+      //     res.send(result)
+      //   })
+      // })
+
   // =============================================================================
   // AUTHENTICATE (FIRST LOGIN) ==================================================
   // =============================================================================
-  
+
       // locally --------------------------------
           // LOGIN ===============================
           // show the login form
           app.get('/login', function(req, res) {
               res.render('login.ejs', { message: req.flash('loginMessage') });
           });
-  
+
           // process the login form
           app.post('/login', passport.authenticate('local-login', {
               successRedirect : '/profile', // redirect to the secure profile section
               failureRedirect : '/login', // redirect back to the signup page if there is an error
               failureFlash : true // allow flash messages
           }));
-  
+
           // SIGNUP =================================
           // show the signup form
           app.get('/signup', function(req, res) {
               res.render('signup.ejs', { message: req.flash('signupMessage') });
           });
-  
+
           // process the signup form
           app.post('/signup', passport.authenticate('local-signup', {
               successRedirect : '/profile', // redirect to the secure profile section
               failureRedirect : '/signup', // redirect back to the signup page if there is an error
               failureFlash : true // allow flash messages
           }));
-  
+
   // =============================================================================
   // UNLINK ACCOUNTS =============================================================
   // =============================================================================
   // used to unlink accounts. for social accounts, just remove the token
   // for local account, remove email and password
   // user account will stay active in case they want to reconnect in the future
-  
+
       // local -----------------------------------
       app.get('/unlink/local', isLoggedIn, function(req, res) {
           var user            = req.user;
@@ -117,14 +151,13 @@ module.exports = function(app, passport, db) {
               res.redirect('/profile');
           });
       });
-  
+
   };
-  
+
   // route middleware to ensure user is logged in
   function isLoggedIn(req, res, next) {
       if (req.isAuthenticated())
           return next();
-  
+
       res.redirect('/');
   }
-  
--- public/main.js
+++ public/main.js
@@ -1,73 +1,85 @@
 
 //client side javascript
+const submit = document.getElementById('submit')
+submit.addEventListener('click', (event) => findWinner(event))
+const bet = document.getElementById("betAmount");
+const display = document.getElementById('display');
+let casinoTotal = 0;
+let wins = 0;
+let losses = 0;
+let result;
+let playerChoice;
 
-var thumbUp = document.getElementsByClassName("fa-thumbs-up");
-var thumbDown = document.getElementsByClassName("fa-thumbs-down");
-var trash = document.getElementsByClassName("fa-trash");
+
+function findWinner(x) {
+  console.log("Running findWinner...");
+  const betAmount = parseInt(bet.value);
+  const winnings = betAmount * 2;
+  const pick = Math.ceil(Math.random() * 2);
+  result = pick === 1 ? 'red' : 'black'
+  const userChoice = document.getElementById('userChoice').value
+  if (userChoice === result) {
+    winner = "player";
+    casinoTotal -= winnings;
+    display.innerHTML = `You won $${winnings}!`;
+    wins += 1
+  } else {
+    winner = "casino";
+    casinoTotal += betAmount;
+    display.innerHTML = `You lost $${betAmount}!`;
+    losses += 1
+  }
+  fetch("/game", {
+    method: "put",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      wins: wins,
+      losses: losses,
+      casinoTotal: casinoTotal,
+    }),
+  })
+  .then(response => {
+    if (response.ok) return response.json()
+  })
+  .then(data => {
+    console.log(data)
+    // window.location.reload(true)
+  })
+}
+
+// let game = {
+//   wins: 0,
+//   losses: 0,
+//   money: 10,000
+// }
+//
+//
+// if( casinioWins === true){
+//   game.money += betAmount
+//   game.wins += 1
+// }else{
+//   game.money -= betAmount
+//   game.losses += 1
+// }
 
 
-Array.from(thumbUp).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
-        fetch('thumbUp', {
-          method: 'put',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg,
-            'thumbUp':thumbUp
-          })
-        })
-        .then(response => {
-          if (response.ok) return response.json()
-        })
-        .then(data => {
-          console.log(data)
-          window.location.reload(true) //reload page
-        })
-      });
-});
-Array.from(thumbDown).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
-        fetch('thumbDown', {
-          method: 'put',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg,
-            'thumbUp':thumbUp
-          })
-        })
-        .then(response => {
-          if (response.ok) return response.json()
-        })
-        .then(data => {
-          console.log(data)
-          window.location.reload(true)
-        })
-      });
-});
 
-Array.from(trash).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        fetch('messages', {
-          method: 'delete',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg
-          })
-        }).then(function (response) {
-          window.location.reload()
-        })
-      });
-});
+// Array.from(trash).forEach(function(element) {
+//       element.addEventListener('click', function(){
+//         const name = this.parentNode.parentNode.childNodes[1].innerText
+//         const msg = this.parentNode.parentNode.childNodes[3].innerText
+//         fetch('messages', {
+//           method: 'delete',
+//           headers: {
+//             'Content-Type': 'application/json'
+//           },
+//           body: JSON.stringify({
+//             'name': name,
+//             'msg': msg
+//           })
+//         }).then(function (response) {
+//           window.location.reload()
+//         })
+//       });
+// });
+//keep the fetch on the button
